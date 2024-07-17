95ef5a5aa843ff3335c78f55247e6a8adbc2f3f5
ahmadgalang <258galang@gmail.com>
2024-07-17 14:34:26 +0700

    logic for button

--- lib/ui/pages/signup_page.dart
+++ lib/ui/pages/signup_page.dart
@@ -1,43 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ticketing/cubit/auth_cubit.dart';
 import 'package:ticketing/shared/theme.dart';
 import 'package:ticketing/ui/widgets/custom_button_widget.dart';
 import 'package:ticketing/ui/widgets/custom_field_form.dart';
 
 class SignUp extends StatelessWidget {
-  const SignUp({super.key});
+  SignUp({super.key});
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController hobbyController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     Widget title() {
       return Container(
         margin: const EdgeInsets.only(bottom: 30),
-        child: Text('Join us and get \nyour next journey',
-            style: blackTextStyle.copyWith(fontSize: 32, fontWeight: semiBold)),
+        child: Text(
+          'Join us and get \nyour next journey',
+          style: blackTextStyle.copyWith(fontSize: 32, fontWeight: semiBold),
+        ),
       );
     }
 
     Widget inputSection() {
       Widget emailInput() {
-        return const CustomFieldForm(label: 'Email Address', placeholder: 'Input Your Email Address',
+        return CustomFieldForm(
+          label: 'Email Address',
+          placeholder: 'Input Your Email Address',
+          controller: emailController,
         );
       }
 
       Widget fullName() {
-        return const CustomFieldForm(label: 'Fullname', placeholder: 'Input Your Fullname',);
+        return CustomFieldForm(
+          label: 'Fullname',
+          placeholder: 'Input Your Fullname',
+          controller: nameController,
+        );
       }
 
       Widget password() {
-        return const CustomFieldForm(label: 'Password', placeholder: 'Input password', obscureText: true );
+        return CustomFieldForm(
+          label: 'Password',
+          placeholder: 'Input password',
+          obscureText: true,
+          controller: passwordController,
+        );
       }
 
       Widget hobby() {
-        return const CustomFieldForm(label: 'Hobby', placeholder: 'Input Your Hobby',);
+        return CustomFieldForm(
+          label: 'Hobby',
+          placeholder: 'Input Your Hobby',
+          controller: hobbyController,
+        );
       }
 
       Widget submitButton() {
-        return CustomButton(titleButton: 'Get Started', margin: const EdgeInsets.only(top: 20),onPressed: () {
-          Navigator.pushNamed(context, '/bonus-page');
-        },);
+        return BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthSuccess) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/bonus-page', (route) => false);
+            } else if (state is AuthFailed) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: kRedColor,
+                  content: Text(state.error),
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return CustomButton(
+              titleButton: 'Get Started',
+              margin: const EdgeInsets.only(top: 20),
+              onPressed: () {
+                 print (passwordController.text);
+                context.read<AuthCubit>().signUp(
+                    email: emailController.text,
+                    password: passwordController.text,
+                    name: nameController.text,
+                    hobby: hobbyController.text);
+              },
+            );
+          },
+        );
       }
 
       return Container(
@@ -82,12 +138,13 @@ class SignUp extends StatelessWidget {
     }
 
     return Scaffold(
-        backgroundColor: kBackgroundColor,
-        body: SafeArea(
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: defaultMargin),
-            children: [title(), inputSection(), tnc()],
-          ),
-        ));
+      backgroundColor: kBackgroundColor,
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: defaultMargin),
+          children: [title(), inputSection(), tnc()],
+        ),
+      ),
+    );
   }
 }
